2899e0c8c62e0dc0368c95913ca7d42c6a7ea19b
anuj <bigdaddy@anujs-MacBook-Air.local>
2022-07-17 08:48:00 +0530

    added cors origin

--- app.js
+++ app.js
@@ -30,17 +30,7 @@ app.use(
   }),
 );
 
-app.use(
-  cors({
-    origin: true,
-    credentials: true,
-    optionsSuccessStatus: 200
-}))
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors({origin: 'http://localhost:3000'}));
 
 
 app.use('/', indexRouter);
